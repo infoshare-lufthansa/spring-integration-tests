755f3528b98f686d8bbe48b80f0b81de029aea20
maciej.koziara <maciej.koziara@airhelp.com>
2021-06-21 22:50:35 +0200

    Improved exercises

--- src/main/java/pl/infoshare/integrationtests/_6_exercise/notes/domain/NoteRepository.java
+++ src/main/java/pl/infoshare/integrationtests/_6_exercise/notes/domain/NoteRepository.java
@@ -11,7 +11,4 @@ public interface NoteRepository extends JpaRepository<Note, Integer> {
     @Query("select n from Note n join n.tags t where t.value in :tags")
     List<Note> findAllByTags(@Param("tags") List<String> tags);
 
-    @Query(value = "select n.id as id, count(t) as totalTags, count(c) as totalComments, split_part(n.value, ' ', 1) from note n left join comment c on n.id = c.note_id left join note_tags nt on n.id = nt.notes_id  left join tag t on nt.tags_id = t.id group by n.id, n.value", nativeQuery = true)
-    List<NoteReport> generateReport();
-
 }
